b75b4d582bbc9775e2514ffcbb87d1cdedacd15f
KyungkuKang <naxsu123@naver.com>
2026-01-13 17:05:29 +0900

    0113 react

--- ex06_redux/src/App.js
+++ ex06_redux/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Route, Routes } from 'react-router-dom';
+import ListPage from './pages/ListPage';
+import LoginPage from './pages/LoginPage';
+import RegPage from './pages/RegPage';
+import IndexPage from './pages/IndexPage';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<>
+  <Routes>
+    <Route path="/" element={<IndexPage />} />
+    <Route path="/list" element={<ListPage />} />
+    <Route path="/login" element={<LoginPage />} />
+    <Route path="/register" element={<RegPage />} />
+  </Routes>
+    {/*<ReduxCon />*/}
+  </>);
 }
 
 export default App;
---
+++ ex06_redux/src/redux-basic/ReduxCom.js
@@ -0,0 +1,8 @@
+const ReduxCom = ({value, upClick, downClick}) => {
+    return (<>
+    cnt.value : {value}<br/>
+    <button onClick={upClick}>upClick</button>
+    <button onClick={downClick}>downClick</button>
+    </>)
+}
+export default ReduxCom;
